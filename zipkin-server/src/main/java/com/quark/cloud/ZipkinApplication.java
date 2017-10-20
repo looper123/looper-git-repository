4c60175bac9917ca3d8c7a5e5ab5c48063d27c09
looper123 <m17701848923@163.com>
2017-10-20 16:34:42 +0800

    spring cloud sleuth + rabbitmq

--- zipkin-server/src/main/java/com/quark/cloud/ZipkinApplication.java
+++ zipkin-server/src/main/java/com/quark/cloud/ZipkinApplication.java
@@ -2,13 +2,16 @@ package com.quark.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import zipkin.server.EnableZipkinServer;
 
 /**
  * Created by ZhenpengLu on 2017/10/20.
  */
 @SpringBootApplication
-@EnableZipkinServer
+//@EnableZipkinServer
+//监听spring.cloud.stream.binder 绑定的消息中间件的获取到的sleuth消息 并且push 倒Zipkin SpanStore
+@EnableZipkinStreamServer
 public class ZipkinApplication {
 
     public static void main(String[] args) {
